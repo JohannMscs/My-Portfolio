c40d54a957859357ce70dae7441f36664a618bb1
JohannMscs <johann.moscoso@gmail.com>
2024-03-24 12:30:01 -0300

    adicionando animacões no componente home

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,27 @@ import Skills from "./components/sections/Skills"
 import Projects from "./components/sections/Projects"
 import Footer from "./components/sections/Footer"
 
+import React, {useEffect} from "react"
+import Aos from "aos"
+import "aos/dist/aos.css"
+
+
 
 //style
 import "./App.sass"
 
 
+
 const App = () => {
+
+  useEffect(() => {
+    Aos.init({
+      duration: 100,
+      easing: 'ease',
+      once: false,
+    })
+  })
+
 
   return(
     <div className="App">
@@ -21,6 +36,11 @@ const App = () => {
     <Skills/>
     <Projects/>
     <Footer/>
+    <div  className="box" id="box1" data-aos="fade-up">box 1</div>
+    <div  className="box" id="box2" data-aos="fade-left">box 2</div>
+    <div  className="box" id="box3" data-aos="fade-left">box 3</div>
+    <div  className="box" id="box5">box 5</div>
+    <div  className="box" id="box6">box 6</div>
     </div>
   )
 }
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,20 +1,53 @@
-//images
-import foto from "../../assets/img/foto.png"
+//hooks
+import { useEffect } from "react";
+import Aos from "aos"
+import "aos/dist/aos.css"
 
 //style
 import "../../assets/sass/components/Home.sass";
 
+//images
+import foto from "../../assets/img/foto.png";
+import github from "../../assets/img/contact/github.png";
+import linkedin from "../../assets/img/contact/linkedin.png";
+import whatsapp from "../../assets/img/contact/whatsapp.png";
+import email from "../../assets/img/contact/email.png";
+
+
+
 // eslint-disable-next-line react/prop-types
 const Home = () => {
 
-  
+  useEffect(() => {
+    Aos.init({
+      duration:1500,
+      easing: 'ease',
+      once: false,
+    })
+  })
 
   return (
-    <div className="home">
-      <img src={foto} alt=""/>
+    <>
+    <div className="home" data-aos="fade-up">
+      <img src={foto} alt="foto" className="foto"  />
       <p>Olá, eu sou sou Johann</p>
       <h1>Desenvolvedor Front-end</h1>
-      </div>
+      <ul >
+        <li>
+        <img src={github} alt="github" />
+        </li>
+        <li>
+        <img src={linkedin} alt="linkedin" />
+        </li>
+        <li>
+        <img src={whatsapp} alt="whatsapp" />
+        </li>
+        <li>
+        <img src={email} alt="email" />
+        </li>
+      </ul>
+    </div>
+    </>
   );
 };
 
