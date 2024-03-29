b4b752c23f6584ee8a5d7d748a12684a9628b66e
JohannMscs <johann.moscoso@gmail.com>
2024-03-29 16:48:45 -0300

    adicionando as imagens das skills ao projeto

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -1,3 +1,5 @@
+//imagem das tecnologias
+import html from '../../assets/img/skills/html.png'
 
 //style
 import "../../assets/sass/components/Skills.sass"
@@ -6,9 +8,35 @@ const Skills = () =>{
      
     return(
 
-        <div>
-            <p>skills</p>
-        </div>
+        <section className='skills'>
+            <h2>skills</h2>
+            <ul>
+                <li id="technologies">
+                    <img src={html} alt="" />
+                </li>
+                <li id="technologies">
+                    <img src="" alt="Css" />
+                </li>
+                <li id="technologies">
+                    <img src="" alt="javascript" />
+                </li>
+                <li id="technologies">
+                    <img src="" alt="typescript" />
+                </li>
+                <li id="technologies">
+                    <img src="" alt="Sass" />
+                </li>
+                <li id="technologies">
+                    <img src="" alt="Tailwind" />
+                </li>
+                <li id="technologies">
+                    <img src="" alt="React" />
+                </li>
+                <li id="technologies">
+                    <img src="" alt="Figma" />
+                </li>
+            </ul>
+        </section>
     )
 }
 
