0d49113b72dd7b1712e8226248c6cb23bd5ea5c4
JohannMscs <johann.moscoso@gmail.com>
2024-04-02 17:59:42 -0300

    estilzação do componente project finalizado para mobile

--- src/components/sections/Project.jsx
+++ src/components/sections/Project.jsx
@@ -1,30 +1,38 @@
 // style
-import '../../assets/sass/components/project.sass'
+import "../../assets/sass/components/project.sass";
 
 //image
-import whiteImage from '../../assets/img/Projects/imagem em branco.png'
+import whiteImage from "../../assets/img/Projects/imagem em branco.png";
 
+const Project = ({ text, projectName, tech_1, tech_2, tech_3, tech_4, github_Repository, project_Link}) => {
+  return (
+    <section className="project_container">
+      <h3>{projectName}</h3>
+      <img src={whiteImage} alt="" className="project_image" />
+      <div>
+        <ul>
+          <li>
+            <img src={tech_1} alt="" />
+          </li>
+          <li>
+            <img src={tech_2} alt="" />
+          </li>
+          <li>
+            <img src={tech_3} alt="" />
+          </li>
+        </ul>
+        <p>{text}</p>
+        <ul className="list_Link">
+          <li className="github_Link">
+            <a href={github_Repository}>Reposito</a>
+          </li>
+          <li className="project_Link">
+            <a href={project_Link}>Projeto</a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
 
-
-
-const Project = ({text, projectName, tech_1, tech_2, tech_3, tech_4}) => {
-
-
-    return(
-        <section className='project_container'>
-            <img src={whiteImage} alt="" className='project_image'/>
-            <div>
-            <ul>
-                <li><img src={tech_1} alt="" /></li>
-                <li><img src={tech_2} alt="" /></li>
-                <li><img src={tech_3} alt="" /></li>
-                
-            </ul>
-            <h3>{projectName}</h3>
-            <p>{text}</p>
-            </div>
-        </section>
-    )
-}
-
-export default Project
+export default Project;
--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -12,18 +12,22 @@ const Projects = ({html, css, javascript, typescript, sass, tailwind, react, fig
         <article>
             <h2>Projetos</h2>
             <Project 
-            projectName={"ola"}
-            text={"seja bem vindo"}
+            projectName={"BookStore"}
+            text={"loja de livros para compra, é possível ver a lista de livros, com mencanismo de filtragem de pesquisa (valores mais caros e mais baratos e gênero), você pode escolher os livros que deseja e adiciona-los ao carrinho para compra"}
             tech_1={html}
             tech_2={css}
             tech_3={javascript}
+            github_Repository={'https://www.google.com'}
+            project_Link={'https://www.google.com'}
             />
             <Project 
-            projectName={"ola"}
-            text={"seja bem vindo"}
+            projectName={"MovieScore"}
+            text={"site de notas de filmes, temos filmes lançamentos e mais antigos, você podecriar uma conta e dar as notas que acha  adequada para determinado filme, assim colaborando com a comunidade"}
             tech_1={html}
             tech_2={css}
             tech_3={javascript}
+            github_Repository={'https://www.youtube.com'}
+            project_Link={'https://www.youtube.com'}
             />
         </article>
     )
